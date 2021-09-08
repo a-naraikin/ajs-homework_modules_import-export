0ed16d927a21cf96f8080e56c19a798909c5033a
anna12god <anna12god@yandex.ru>
2021-09-08 19:49:10 +0300

    completed the task:Import/Export

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,7 @@
 // TODO: write your code here
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js'
+
+const game = new Game();
+game.start();
+
 console.log('app worked');
